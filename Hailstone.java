d41c23dc12a74756e05e4f7bf387e48064df0e1c
S <s>
2016-07-01 13:22:59 +0800

    {"types":"C","runs":6}

--- Hailstone.java
+++ Hailstone.java
@@ -1,5 +1,9 @@
 /* The program is to compute a number ,show the process of the hailstone.
- * hailstone number: 
+ * hailstone number: Start with any positive integer (an initial seed)
+ * and obtain a sequence of numbers by following these rules.
+ * 1. If the current number is even, divide it by two   ; 
+ *   else if it is odd, multiply it by three and add one. 
+ * 2. Repeat.
  * 
  */
 import acm.program.*;
@@ -7,16 +11,16 @@ import acm.program.*;
 public class Hailstone extends ConsoleProgram {
 	public void run() {
 		int number = readInt("Please input an positive Integer:");
-		if(number!=1){
-			if(number%2==0){
-				even(number);
-			}else{
-				odd(number);
-			}
+		if(number%2==0){
+			even(number);
+		}else{
+			odd(number);
 		}
-		
 	}
 
+	/*
+	 * when the number is even,divide it by two.
+	 */
 	private void even(int number) {
 		
 		println(number+"  is even,so I make half:	"+number/2);
