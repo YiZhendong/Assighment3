1beeff9d675074f4e7f8a281e9b09aaa21df2326
S <s>
2016-07-01 15:50:19 +0800

    {"types":"C","runs":2}

--- Pyramid.java
+++ Pyramid.java
@@ -12,9 +12,9 @@ public class Pyramid extends GraphicsProgram {
 	private static final double BRICK_HEIGHT = 12;
 
 	/** Number of bricks in the base of the pyramid */
-	private static final double BRICKS_IN_BASE = 14;
+	private static final int BRICKS_IN_BASE = 14;
 	
-	private static int originalX,OriginalY;
+	private static double originalX,OriginalY;
 	public void run() {
 		for(int i=0;i<BRICKS_IN_BASE;i++){
 			createBrickRow(i);
@@ -33,6 +33,7 @@ public class Pyramid extends GraphicsProgram {
 	private void getOriginalCoordinate(int i) {
 		// TODO Auto-generated method stub
 		originalX = getWidth() - (i+1)*BRICK_WIDTH;
+		originalY = getHeight() - BRICK_HEIGHT;
 	}
 }
 
