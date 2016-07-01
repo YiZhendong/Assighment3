c021e3bc20c40e11440d78354368b2b797b341da
S <s>
2016-07-01 11:28:58 +0800

    {"types":"C","runs":19}

--- AerobicExercise.java
+++ AerobicExercise.java
@@ -5,8 +5,18 @@ import acm.program.*;
 
 public class AerobicExercise extends ConsoleProgram {
 	public void run() {
+		inputMinutes();
+		showAdvice();
+	}
+
+	private void inputMinutes() {
 		for(int i = 1 ;i < 8; i++){
-			
+			int value = readInt("How many minutes of asrobic Execrcise did you do on day "+i+":");
 		}
+	}
+
+	private void showAdvice() {
+		// TODO Auto-generated method stub
+		
 	}
 }
