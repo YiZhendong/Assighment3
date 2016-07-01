df91b8a8d4658b8cb32a15c31696bad858904f87
S <s>
2016-07-01 11:42:21 +0800

    {"types":"C","runs":22}

--- AerobicExercise.java
+++ AerobicExercise.java
@@ -35,7 +35,7 @@ public class AerobicExercise extends ConsoleProgram {
 		if (bloHealthTimes >= 2){
 			println("	Good job,you have done enough exercise for blood exercise");
 		}else{
-			println("	You needed 40 or more minutes of blood exercise on at"+ (3-carHealthTimes) +" more day(s)");
+			println("	You needed 40 or more minutes of blood exercise on at"+ (3-bloHealthTimes) +" more day(s)");
 		}
 	}
 }
