fa83cc0b619e86e28bb04a9a91bdaedf131a2c33
S <s>
2016-07-01 11:41:30 +0800

    {"types":"C","runs":21}

--- AerobicExercise.java
+++ AerobicExercise.java
@@ -18,8 +18,8 @@ public class AerobicExercise extends ConsoleProgram {
 	private void inputMinutes() {
 		for(int i = 1 ;i < 8; i++){
 			int value = readInt("How many minutes of asrobic Execrcise did you do on day "+i+":");
-			if (value > 30) carHealthTimes ++;
-			if (value > 40) bloHealthTimes ++;
+			if (value >= 30) carHealthTimes ++;
+			if (value >= 40) bloHealthTimes ++;
 		}
 	}
 
