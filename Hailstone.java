95564f1222324ddcb501176f64cf5675bfa771d9
S <s>
2016-07-01 13:07:56 +0800

    {"types":"C","runs":1}

--- Hailstone.java
+++ Hailstone.java
@@ -5,7 +5,27 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
+		int number = readInt("Please input an positive Integer:");
+		while (number!=1){
+			if(number%2==0){
+				even(number);
+			}else{
+				odd(number);
+			}
+		}
+		
+	}
 
+	private int even(int number) {
+		// TODO Auto-generated method stub
+		println(number+"  is even,so I make half:	"+number/2);
+		number /=2;
+		return number;
+	}
+
+	private void odd(int number) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
