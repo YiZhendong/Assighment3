9fc6e5e19e53414a94c0dfde886e059adc5f9299
S <s>
2016-07-01 16:11:01 +0800

    {"types":"C","runs":9}

--- SaintPetersburgGame.java
+++ SaintPetersburgGame.java
@@ -2,9 +2,26 @@
  * does.
  */
 import acm.program.*;
+import acm.util.RandomGenerator;
 
 public class SaintPetersburgGame extends ConsoleProgram {
+	/** The total number*/
+	public static int total = 0;
+	
 	public void run() {
-		
+		while(total<=20){
+			playGame();
+		}
+	}
+
+	private void playGame() {
+		// TODO Auto-generated method stub
+		Boolean comesUpHead = true;
+		int num = 0;
+		while(comesUpHead){
+			RandomGenerator rgen = RandomGenerator.getInstance();
+			comesUpHead = rgen.nextBoolean();
+			num++;
+		}
 	}
 }
