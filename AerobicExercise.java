a78c2a8bf6ae9d860264e02f92f59e0e0a700eef
S <s>
2016-07-01 11:35:22 +0800

    {"types":"C","runs":20}

--- AerobicExercise.java
+++ AerobicExercise.java
@@ -6,16 +6,16 @@ import acm.program.*;
 public class AerobicExercise extends ConsoleProgram {
 	
 	/*represent the days of exercise whose time is longer than 30 and 40*/
-	private static final int carHealthTimes = 0;
-	private static final int bloHealthTimes = 0;
+	private static int carHealthTimes = 0;
+	private static int bloHealthTimes = 0;
 	public void run() {
 		
 		inputMinutes();
 		showAdvice();
 	}
 
+	/*input the minutes */
 	private void inputMinutes() {
-
 		for(int i = 1 ;i < 8; i++){
 			int value = readInt("How many minutes of asrobic Execrcise did you do on day "+i+":");
 			if (value > 30) carHealthTimes ++;
@@ -24,7 +24,6 @@ public class AerobicExercise extends ConsoleProgram {
 	}
 
 	private void showAdvice() {
-		// TODO Auto-generated method stub
 		
 	}
 }
